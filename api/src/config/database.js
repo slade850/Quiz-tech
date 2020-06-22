539ce367d8dab45aa366036e01ffd5464ac6b312
Julien <julien.bardin81@gmail.com>
2020-06-22 16:58:46 +0200

    [API] add db port for database connection

--- api/src/config/database.js
+++ api/src/config/database.js
@@ -1,6 +1,7 @@
 import mysql from 'mysql2';
 
 const connection = mysql.createConnection({
+    port: process.env.DB_PORT,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
