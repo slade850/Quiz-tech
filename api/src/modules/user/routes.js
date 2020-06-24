098c31de8776d19c248fcc9be4141579af459cf9
rimIlmia <rim.chakroun.pro@gmail.com>
2020-06-25 01:27:46 +0200

    add default avatar+ get userwit avatar

--- api/src/modules/user/routes.js
+++ api/src/modules/user/routes.js
@@ -1,10 +1,10 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
-import userController from './controller'
+import userController from "./controller";
 
-router.get("/", (req, res)=> res.send('hello'));
-router.post('/register', userController.register);
-router.post('/login', userController.login);
-
+router.get("/", (req, res) => res.send("hello"));
+router.post("/register", userController.register);
+router.post("/login", userController.login);
+router.get("/:id", userController.getById);
 
 export default router;
