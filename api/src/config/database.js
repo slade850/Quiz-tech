d2b3fffd1b1e94243b133cc8f6fa0ffee07e1e98
shelley cavaness <shelleycavaness@yahoo.com>
2020-06-24 15:38:13 +0200

    Update database.js

--- api/src/config/database.js
+++ api/src/config/database.js
@@ -3,10 +3,9 @@ import mysql from 'mysql2';
 const connection = mysql.createConnection({
     port: process.env.DB_PORT,
     host: process.env.DB_HOST,
-        user: "root",
-    // user: process.env.DB_USER,
-    // password: process.env.DB_PASS,
-    database: "quiz-tech"
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_BASE
 });
 
 //Connecting to database
