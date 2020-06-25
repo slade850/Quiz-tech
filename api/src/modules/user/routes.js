9b2c983b80d92b683e3eb19c8b67a0d5cc822646
Julien <julien.bardin81@gmail.com>
2020-06-25 11:08:37 +0200

    add start front

--- api/src/modules/user/routes.js
+++ api/src/modules/user/routes.js
@@ -1,10 +1,11 @@
 import express from "express";
 const router = express.Router();
 import userController from "./controller";
+import autorize from "../../helper/autorize";
 
 router.get("/", (req, res) => res.send("hello"));
 router.post("/register", userController.register);
 router.post("/login", userController.login);
-router.get("/:id", userController.getById);
+router.get("/:id", autorize(['player']), userController.getById);
 
 export default router;
