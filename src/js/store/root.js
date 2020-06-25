3d922b4e27bb41d61859a8a209d672f2a6c70c76
rimIlmia <rim.chakroun.pro@gmail.com>
2020-06-25 17:01:48 +0200

    ok

--- src/js/store/root.js
+++ src/js/store/root.js
@@ -1,10 +1,11 @@
 import { combineReducers } from "redux";
 import authStore from "./authStore";
 import themeStore from "./themeStore";
-
+import questionStore from "./questionStore";
 const creatRootReducer = combineReducers({
   authStore,
   themeStore,
+  questionStore,
 });
 
 export default creatRootReducer;
