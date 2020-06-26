7ac2c979b6d7493fcc4bc928cfc99131622d77a5
rimIlmia <rim.chakroun.pro@gmail.com>
2020-06-26 12:17:16 +0200

    test

--- src/js/utils/api.js
+++ src/js/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000/api/",
+  baseURL: "http://localhost:3001/api/",
 });
 export const addAuth = (token) => {
   instance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
